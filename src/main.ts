def7c858003caead6b7c685d2d53eea3a0c85bc4
Kyeong1024 <steven@netstream.co.kr>
2022-09-08 15:09:42 +0900

    Add: hot reloading

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: `http://localhost:${process.env.PORT}`,
+      credentials: true,
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
